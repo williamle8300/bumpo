ca6e49165eaa296dcff9d933ef38b9cc5d14b4d0
William Le <wle8300@gmail.com>
2017-06-14 12:48:55 -0700

    asdf

---
+++ src/sampleData.js
@@ -0,0 +1,54 @@
+const sampleData = [
+  {
+    id: 'a',
+    text: 'pay SCE electric bill',
+    score: 0,
+  },
+  {
+    id: 'b',
+    text: 'print out Psalm 27',
+    score: 0,
+  },
+  {
+    id: 'c',
+    text: 'print auto insurance card',
+    score: 0,
+  },
+  {
+    id: 'd',
+    text: 'dog tax',
+    score: 0,
+  },
+  {
+    id: 'e',
+    text: 'kill cockroaches',
+    score: 0,
+  },
+  {
+    id: 'f',
+    text: 'double backup fig',
+    score: 0,
+  },
+  {
+    id: 'g',
+    text: 'fix pub.center',
+    score: 0,
+  },
+  {
+    id: 'h',
+    text: 'reply to Neutab',
+    score: 0,
+  },
+  {
+    id: 'i',
+    text: 'haircut',
+    score: 0,
+  },
+  {
+    id: 'j',
+    text: 'pay toll fee',
+    score: 0,
+  },
+]
+
+export default sampleData
